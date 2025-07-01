69c8b10a935b2379dd29f80451f73c3e25aca1f9
EnriqueBravo115 <enriquebravo115@gmail.com>
2025-07-01 09:56:03 -0600

    initial

---
+++ README.md
@@ -0,0 +1,1 @@
+Para ejecutar el proyecto usar Clj -A:dev lo que te pondra en el ns dev, luego en un REPL insertar:
---
+++ dev/dev.clj
@@ -0,0 +1,11 @@
+(ns dev
+  (:require [com.stuartsierra.component.repl :as component-repl]
+            [api.core :as core]))
+
+(component-repl/set-init
+  (fn [_]
+    (core/real-world-clojure-api-system
+      {:server {:port 3001}
+       :db-spec {:jdbcUrl "jdbc:postgresql://localhost:5432/rwca"
+                 :username "rwca"
+                 :password "rwca"}})))
