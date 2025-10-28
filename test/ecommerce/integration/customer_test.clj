56439cce67488dfb6413b9d798dccb82b67e3c7c
EnriqueBravo115 <enriquebravo115@gmail.com>
2025-10-28 13:53:04 -0600

    add 1 test on customer_test

--- test/ecommerce/integration/customer_test.clj
+++ test/ecommerce/integration/customer_test.clj
@@ -15,7 +15,7 @@
          (component/stop ~bound-var)))))
 
 (deftest get-customer-by-id
-  (testing "first test"
+  (testing "GET /api/v1/customer/:id should return customer"
     (with-system
       [sut (system/system-component {:server {:port 3000}
                                      :db-spec {:jdbcUrl "jdbc:postgresql://localhost:5432/ecommerce"
@@ -32,3 +32,23 @@
                 :email "maria.garcia@email.com"
                 :active true}
                customer))))))
+
+(deftest get-customer-country-count
+  (testing "GET /api/v1/customer/country-count should return country count aggregation"
+    (with-system
+      [sut (system/system-component {:server {:port 3000}
+                                     :db-spec {:jdbcUrl "jdbc:postgresql://localhost:5432/ecommerce"
+                                               :username "ecommerce"
+                                               :password "ecommerce"}})]
+      (let [response (client/get "http://localhost:3000/api/v1/customer/country-count" {:accept :json})
+            body (-> response :body (cheshire/parse-string true))
+            country_count (:country_count body)]
+
+        (is (= 200 (:status response)))
+        (is (vector? country_count))
+        (let [country-map
+              (into {} (map (fn [item] [(:country_of_birth item) (:count item)]) country_count))]
+          (is (= 1 (get country-map "COL")))
+          (is (= 1 (get country-map "ESP")))
+          (is (= 2 (get country-map "MEX")))
+          (is (= 1 (get country-map "USA"))))))))
